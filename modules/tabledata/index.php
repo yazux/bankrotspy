b330f30fe4757aeba8fe6a4f4a05fd9593ccbf69
web_demon <demon.of.net@gmail.com>
2015-03-23 22:08:47 +0300

    Отмеченный чекбокс "Рыночная цена" ничего не меняет если не заданы интевалы цен

--- modules/tabledata/index.php
+++ modules/tabledata/index.php
@@ -127,8 +127,11 @@ if($price_start)
 if($price_end)
   $conditions['price_end'] = ' `ds_maindata`.`'.$price_search.'` < "' . $price_end . '" ';
 
-if($type_price == 3)
-  $conditions['price_end_third'] = ' `ds_maindata`.`'.$price_search.'` > "0" ';
+if($price_start OR $price_end)
+{
+  if($type_price == 3)
+    $conditions['price_end_third'] = ' `ds_maindata`.`' . $price_search . '` > "0" ';
+}
 
 //Компилим условия
 $where_cond = '';
