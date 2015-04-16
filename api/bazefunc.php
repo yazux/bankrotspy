9c5bbed4862a0137d359c06ce90e4cc79fb8ea5a
web_demon <demon.of.net@gmail.com>
2015-04-17 00:40:44 +0300

    Сделан контролер для апи

---
+++ api/bazefunc.php
@@ -0,0 +1,50 @@
+<?php
+defined('DS_ENGINE') or die('web_demon laughs');
+
+function denied()
+{
+  list($file_error) = debug_backtrace();
+  $file_r = str_replace('\\','/',$file_error['file']);
+  $file_r = str_replace($_SERVER['DOCUMENT_ROOT'],'',$file_r);
+  echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru"><body>';
+  echo '<b>ACCESS ERROR!</b> Denied() called in "'.$file_r.'" <b>on line</b> '.$file_error['line'].'</body></html>';
+  exit();
+
+  //header('Location: ' . core::$home);
+  exit();
+}
+
+function GET($string)
+{
+  if(isset($_GET[$string]))
+    return trim($_GET[$string]);
+  return FALSE;
+}
+
+function smart_trim($string)
+{
+  $string = str_replace("\r\n", "\n", $string);
+  $first_pos = mb_strpos($string, "\n");
+  if($first_pos === false)
+    return trim($string); //Если в строке нет переносов, тримим обычной функцией
+  else
+  { //Ну а если есть, то удаляем в начале ТОЛЬКО пустые строки, проблелы в начале строки не трогаем
+    while(trim(mb_substr($string, 0, ($first_pos+1))) == '')
+    {
+      $string = mb_substr($string, ($first_pos+1));
+      $first_pos = mb_strpos($string, "\n");
+    }
+    return rtrim($string); //В конце строки удаляем все
+  }
+}
+
+function POST($string)
+{
+  if(isset($_POST[$string]))
+  {
+    if (is_array($_POST[$string]))
+      return $_POST[$string];
+    return smart_trim($_POST[$string]);
+  }
+  return FALSE;
+}
---
+++ api/versions/1.0/index/index.php
@@ -0,0 +1,5 @@
+<?php
+defined('DS_ENGINE') or die('web_demon laughs');
+
+
+echo 'Version: '.core::$apiversion;
