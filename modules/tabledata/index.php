b6adfe77963af4ed6ff176862d74113f8baabfa5
Alekasandr <imbagroup@yandex.ru>
2015-10-24 12:19:42 +0300

    update

--- modules/tabledata/index.php
+++ modules/tabledata/index.php
@@ -205,6 +205,7 @@ if($price_start OR $price_end)
 //выборка новых лотов за последние 48 часов
 if(!empty($new_lots)) {
     $conditions['loadtime'] = ' FROM_UNIXTIME(`ds_maindata`.`loadtime`) > NOW() - INTERVAL 2 DAY ';
+    $order_cond = ' ORDER BY `ds_maindata`.`loadtime` DESC ';
 }
 
 //Компилим условия
